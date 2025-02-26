e0b11ba47b7ab863833b5919ef4c65cc8a5ad334
cfcd3 <79826895+cfcd3@users.noreply.github.com>
2025-02-27 01:16:24 +0800

    Update bank.py
    improved the modularity by importing SavingsAccount from a separate file.

--- LR3/PostLabSolution1/bank.py
+++ LR3/PostLabSolution1/bank.py
@@ -1,4 +1,6 @@
 #DYTIANQUIN, CHALZEA FRANSEN C.
+from savingsaccount import SavingsAccount
+
 class Bank:
     def __init__(self):
         self.accounts = []
@@ -20,18 +22,6 @@ class Bank:
 
         # Print bank's accounts
         print(self)
-
-class SavingsAccount:
-    def __init__(self, account_holder_name, balance=0):
-        self.account_holder_name = account_holder_name
-        self.balance = balance
-
-    def __lt__(self, other):
-        return self.account_holder_name < other.account_holder_name
-
-    def __str__(self):
-        return f"{self.account_holder_name}: ${self.balance:.2f}"
-
 
 if __name__ == "__main__":
     bank = Bank()
